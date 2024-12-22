c9bfad6880827314c676d4650f3efd84ec10a166
VladKhurs <khursvladislav@gmail.com>
2024-12-22 18:16:56 +0300

    feat: teature add-car-image

--- src/app/car/add-car/Cars.tsx
+++ src/app/car/add-car/Cars.tsx
@@ -153,7 +153,7 @@ export default function Cars() {
               ) : (
                 <>
                   <label>Фотографии</label>
-                  <label>Количество добавленных фото: {photos.length}/8</label>
+                  <label className={`${styles.fontGrayThin} ${styles.imagesCounter}`}>Загружено: {photos.length}/8</label>
                 </>
               )}
             </div>
@@ -185,40 +185,59 @@ export default function Cars() {
                   </button>
                 </div>
               )}
-              {photos.map((photo, index) => (
-                <div key={index} className={styles.photoContainer}>
-                  <img
-                    src={URL.createObjectURL(photo)}
-                    alt={`Фото ${index + 1}`}
-                  />
-                  <button
-                    type="button"
-                    onClick={() => handleRemovePhoto(index)}
+              <div className={styles.photosWrapper}>
+                {photos.map((photo, index) => (
+                  <div key={index} className={styles.photoContainer}>
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt={`Фото ${index + 1}`}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => handleRemovePhoto(index)}
+                    >
+                      <svg
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M1.46484 8.53484L8.53684 1.46484M1.46484 1.46484L8.53684 8.53484"
+                          stroke="black"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                ))}
+                {photos.length > 0 && photos.length < 8 && (
+                  <div 
+                  className={styles.addMorePhotos}
+                  onClick={() =>
+                    document.querySelector('input[type="file"]')?.click()
+                  }
                   >
-                    ✖
-                  </button>
-                </div>
-              ))}
-              {photos.length > 0 && photos.length < 8 && (
-                <div>
-                  <p>Выберите или перетащите фотографии в область</p>
-                  <p>Форматы JPEG, JPG или PNG до 10 МБ каждый</p>
-                  <input
-                    type="file"
-                    accept="image/jpeg,image/jpg,image/png"
-                    multiple
-                    onChange={handleAddPhoto}
-                  />
-                  <button
-                    type="button"
-                    onClick={() =>
-                      document.querySelector('input[type="file"]')?.click()
-                    }
-                  >
-                    Выбрать фотографии
-                  </button>
-                </div>
-              )}
+
+<label>
+<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.3333 8.22222H18.3478M1 5.33333C1 4.18406 1.45655 3.08186 2.2692 2.2692C3.08186 1.45655 4.18406 1 5.33333 1H22.6667C23.8159 1 24.9181 1.45655 25.7308 2.2692C26.5435 3.08186 27 4.18406 27 5.33333V22.6667C27 23.8159 26.5435 24.9181 25.7308 25.7308C24.9181 26.5435 23.8159 27 22.6667 27H5.33333C4.18406 27 3.08186 26.5435 2.2692 25.7308C1.45655 24.9181 1 23.8159 1 22.6667V5.33333Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1 19.7775L8.22222 12.5553C9.56267 11.2654 11.2151 11.2654 12.5556 12.5553L19.7778 19.7775" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.8887 16.8884L18.3331 15.444C19.6736 14.1541 21.326 14.1541 22.6664 15.444L26.9998 19.7773" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</label>
+                    <label>Добавить</label>
+                    <input
+                      type="file"
+                      accept="image/jpeg,image/jpg,image/png"
+                      multiple
+                      onChange={handleAddPhoto}
+                    />
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </form>
