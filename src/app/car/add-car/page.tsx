fbfe08c9920576f41c99ce8e32de4ad5c32e2f9e
Dian A <dianakononovic4@gmail.com>
2025-01-07 15:01:21 +0300

    import reset.css file

--- src/app/car/add-car/page.tsx
+++ src/app/car/add-car/page.tsx
@@ -1,14 +1,14 @@
 import FormCar from "./FormCar";
 import TableCar from "./TableCar";
 import styles from "./Cars.module.css";
+import Cars from "./Cars";
 
 export default function CarsPage() {
   return (
     <div className={styles.items}>
       <div className={styles.main_form}>
         <div className={styles.container}>
-          <FormCar />
-          <TableCar />
+          <Cars />
         </div>
       </div>
     </div>
