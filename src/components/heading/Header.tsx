159910da18bdc811271c77ac0330f9fc75d76903
Roman Chechyotkin <73552299+romanchechyotkin@users.noreply.github.com>
2025-01-13 16:45:14 +0300

    Header based on user type (#38)
    * Header based on user type
    
    * Changed business role
    
    * Chenged customer and superuser roles
    
    * подредачи хедер
    
    ---------
    
    Co-authored-by: Dian A <dianakononovic4@gmail.com>
    Co-authored-by: Chechetkin Roman Aleksandrovich <rchechetkin@ozon.ru>

---
+++ src/components/heading/Header.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { getUserStorage, removeFromStorage } from '@/services/auth-token.service'
+import { useRouter } from 'next/navigation'
+import { IUser } from '@/types/auth.type'
+
+export default function Header() {
+	const router = useRouter()
+
+	const user: IUser | null = getUserStorage()
+
+	const logout = () => {
+		removeFromStorage()
+		router.replace('/');
+	}
+
+	return (
+		<div className='h-20 flex justify-between items-center'>
+			<div>
+				<img
+					className='m-auto'
+					src="/logo_tachki.svg"
+					alt="logo"
+				/>
+			</div>
+			<nav className='font-medium'>
+				{user?.user_type === 'admin' || user?.user_type === 'worker' ? (
+					<>
+                        <a href='#'>Автопарк</a>
+				        <a href='#'>Запросы на аренду</a>	
+					</>
+				) : user?.user_type === 'customer' ? (
+                    <>
+                    </>
+				) : (
+                    <>
+                        <a href='#'>Компании</a>
+						<a href='#'>Верификация</a>
+                    </>
+				)}
+			</nav>
+
+			<button
+				className='w-48 h-12 bg-errorRed text-white font-medium text-base rounded-xl'
+				onClick={logout}
+			>
+				Выйти
+			</button>
+		</div>
+	)
+}
