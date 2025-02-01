a35e6dfa697f630f238974c0dc6ae21e0f7a7375
VladKhurs <khursvladislav@gmail.com>
2025-02-01 17:52:49 +0300

    feat: clean fields and redirect after create car

--- src/app/dashboard/business/create/Cars.tsx
+++ src/app/dashboard/business/create/Cars.tsx
@@ -1,15 +1,16 @@
 "use client";
 import styles from "./Cars.module.css";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { CarService } from "@/services/car.service";
 import { DASHBOARD_PAGES } from "@/config/pages-url.config";
 import Link from "next/link";
 import { carBrandData, carClassData, carTransmissionsData } from "@/utils/constants";
+import { useRouter } from 'next/navigation';
 
 export default function Cars() {
   // TODO add it when will be needed
   //const dispatch = useAppDispatch();
-
+  const router = useRouter();
   const [carBrand, setCarBrand] = useState("");
   const [carModel, setCarModel] = useState("");
   const [carNumber, setCarNumber] = useState("");
@@ -20,8 +21,6 @@ export default function Cars() {
   const [carDescription, setCarDescription] = useState("");
   const [photos, setPhotos] = useState<File[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const [addCarData, setAddCarData] = useState({});
 
   const [isFieldPreparing, setIsFieldPreparing] = useState<boolean>(true);
 
@@ -35,10 +34,22 @@ export default function Cars() {
     formData.append("price_per_day", carPrice.toString());
     formData.append("transmission", carTransmission);
     formData.append("year", carYear.toString());
-
     photos.forEach((photo) => {
       formData.append(`image`, photo);
     });
+
+    router.replace(DASHBOARD_PAGES.BUSINESS_CARS);
+
+    setIsFieldPreparing(true)
+    setCarBrand(()=>"")
+    setCarModel(()=> "")
+    setCarNumber(()=> "")
+    setCarTransmission(()=> "")
+    setCarClass(()=> "")
+    setCarPrice(()=> 1)
+    setCarYear(()=> 2010)
+    setCarDescription(()=> "")
+    setPhotos(()=> [])
 
     try {
       const createdCar = await CarService.createCar(formData);
@@ -74,26 +85,10 @@ export default function Cars() {
       carDescription &&
       photos.length
     ) {
-      setAddCarData({
-        brand: carBrand,
-        class: carClass,
-        description: carDescription,
-        images: photos,
-        model: carModel,
-        price_per_day: carPrice,
-        transmission: carTransmission,
-        year: carYear,
-      });
-      // TODO add it when will be needed
-      //dispatch(setCar(addCarData));
+      createCar();
+
     }
   };
-
-  useEffect(() => {
-    console.log("--------------------------------------");
-    createCar();
-    console.log("addCarData", addCarData);
-  }, [addCarData]);
 
   const handleAddPhoto = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
