f3a5e9a5135724b3db0048f42629bce857059ee1
Diana Kononovich <149074930+imsiteb@users.noreply.github.com>
2025-02-01 14:16:14 +0300

    Car information (#57)
    * Aded slider, fixed problem w header
    
    * Corrected mistakes and added adaptive design
    
    * Replaced types in car service

--- src/components/heading/Header.tsx
+++ src/components/heading/Header.tsx
@@ -4,56 +4,72 @@ import { useRouter } from 'next/navigation'
 import { IUser } from '@/types/auth.type'
 import { DASHBOARD_PAGES } from '@/config/pages-url.config'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import Loader from '../Loader'
+import { LogOut } from 'lucide-react'
 
 
 export default function Header() {
 	const router = useRouter()
 
-	const user: IUser | null = getUserStorage()
+	const [user, setUser] = useState<IUser | null>(null)
+
+	useEffect(() => {
+		const userData = getUserStorage()
+		console.log("Данные пользователя из cookies:", userData)
+		setUser(userData)
+	}, [])
 
 
 	const logout = () => {
 		removeFromStorage()
-		router.replace('/');
+		router.replace('/')
 	}
 
 	return (
 		<div className='h-20 flex justify-between items-center'>
-		
-		  <div>
-			<img
-			  className='m-auto'
-			  src="/logo_tachki.svg"
-			  alt="logo"
-			/>
-		  </div>
-	
 
-		  <nav className='font-medium flex gap-4'>
-			{user?.user_type === 'admin' || user?.user_type === 'worker' ? (
-			  <>
-				<a href={DASHBOARD_PAGES.BUSINESS_CARS}>Автопарк</a>
-				<a href='#'>Запросы на аренду</a>
-			  </>
-			) : user?.user_type === 'customer' ? (
-			  <>
-			
-			  </>
-			) : (
-			  <>
-				<a href='#'>Компании</a>
-				<a href='#'>Верификация</a>
-			  </>
-			)}
-		  </nav>
-	
-		  <button
-			className='w-48 h-12 bg-errorRed text-white font-medium text-base rounded-xl'
-			onClick={logout}
-		  >
-			Выйти
-		  </button>
+			<div>
+				<img
+					className='m-auto 2sm-max:w-20'
+					src="/logo_tachki.svg"
+					alt="logo"
+				/>
+			</div>
+
+
+			<nav className='font-medium flex gap-4'>
+				{user?.user_type === 'admin' || user?.user_type === 'worker' ? (
+					<>
+						<Link href={DASHBOARD_PAGES.BUSINESS_CARS}>Автопарк</Link>
+						<Link href='#'>Запросы на аренду</Link>
+					</>
+				) : user?.user_type === 'customer' ? (
+					<>
+
+					</>
+				) : user?.user_type === 'superuser' ? (
+					<>
+						<Link href='#'>Компании</Link>
+						<Link href='#'>Верификация</Link>
+					</>
+				) : <Loader />}
+			</nav>
+
+			<button
+				className='w-48 h-12 bg-errorRed text-white font-medium text-base rounded-xl 2sm-max:hidden'
+				onClick={logout}
+			>
+				Выйти
+			</button>
+
+			<div className='2sm-max:block hidden'>
+				<LogOut
+					className='text-errorRed w-8 h-8 cursor-pointer'
+					onClick={logout}
+				/>
+			</div>
 		</div>
-	  );
+	)
 }
 
