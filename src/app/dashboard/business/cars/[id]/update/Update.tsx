92d760cdd4647a4d6036e553440672da45907854
VladKhurs <khursvladislav@gmail.com>
2025-02-01 16:59:30 +0300

    feat: update update-cer-logic and fill prev data

--- src/app/dashboard/business/cars/[id]/update/Update.tsx
+++ src/app/dashboard/business/cars/[id]/update/Update.tsx
@@ -10,25 +10,26 @@ import { useCar } from "../../hooks/useCar";
 export default function Update() {
   const { id } = useParams()
   const carId = Array.isArray(id) ? id.join('') : id || ''
+    const { data, isLoading } = useCar(carId)
   const [carPrice, setCarPrice] = useState(1);
-  const [carDescription, setCarDescription] = useState("");
-  const [updateCarData, setUpdateCarData] = useState({});
+  const [carDescription, setCarDescription] = useState('!!')
+  const [carYear, setCarYear] = useState(1);
+
   const [isFieldPreparing, setIsFieldPreparing] = useState<boolean>(true);
-  const { data, isLoading } = useCar(carId)
 
   useEffect(()=> {
-      console.log('data', data)
-  }, [data])
-
-  useEffect(() => {
-    updateCar();
-    console.log("updateCarData", updateCarData);
-  }, [updateCarData]);
+      if(!isLoading) {
+          setCarPrice(()=>data?.data.car.price_per_day)
+          setCarYear(()=>data?.data.car.year)
+          setCarDescription(()=>data?.data.car.description)
+      }
+  }, [isLoading])
 
   const updateCar = async () => {
     const carData = {
       description: carDescription,
-      price_per_day: carPrice
+      price_per_day: carPrice,
+      year: carYear.toString()
     };
 
     const carDataJson = JSON.stringify(carData)
@@ -45,18 +46,17 @@ export default function Update() {
     setIsFieldPreparing(true);
     setCarPrice(0);
     setCarDescription("");
+    setCarYear(2010);
   };
 
   const handleUpdateCar = () => {
     setIsFieldPreparing(false);
     if (
       carPrice &&
-      carDescription
+      carDescription &&
+        carYear
     ) {
-      setUpdateCarData({
-        description: carDescription,
-        price_per_day: carPrice,
-      });
+        updateCar();
     }
   };
 
@@ -74,32 +74,56 @@ export default function Update() {
           : data?.data.car.transmission}
       </h1>
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
       >
         <div className={`flex flex-row flex-wrap items-center gap-4 mb-5 mt-5`}>
           <div className={styles.inputWrap}>
             <label className={styles.labelBottom}>Цена (BYN в сутки)</label>
             <input
-              type="number"
-              value={carPrice}
-              placeholder={"Введите цену"}
-              className={`${styles.customInput} mt-2 w-full items-center border-2 border-grey bg-transparent p-4 font-light text-base outline-none placeholder:text-grey placeholder:font-normal duration-500 transition-colors focus:border-primary`}
-              onChange={(e) => {
-                if (Number(e.target.value) < 1) {
-                  e.target.value = "1";
+                type="number"
+                value={carPrice}
+                placeholder={"Введите цену"}
+                className={`${styles.customInput} mt-2 w-full items-center border-2 border-grey bg-transparent p-4 font-light text-base outline-none placeholder:text-grey placeholder:font-normal duration-500 transition-colors focus:border-primary`}
+                onChange={(e) => {
+                  if (Number(e.target.value) < 1) {
+                    e.target.value = "1";
+                  }
+                  setCarPrice(Number(e.target.value));
+                }}
+                style={
+                  (isFieldPreparing || carPrice) === 1
+                      ? {
+                        backgroundColor: "rgba(255,0,0,0.025)",
+                        border: "2px solid red",
+                      }
+                      : {}
                 }
-                setCarPrice(Number(e.target.value));
-              }}
-              style={
-                (isFieldPreparing || carPrice) === 1
-                  ? {
-                      backgroundColor: "rgba(255,0,0,0.025)",
-                      border: "2px solid red",
-                    }
-                  : {}
-              }
+            />
+          </div>
+          <div className={styles.inputWrap}>
+            <label className={styles.labelBottom}>Год выпуска</label>
+            <input
+                type="number"
+                value={carYear}
+                className={`${styles.customInput} mt-2 w-full items-center border-2 border-grey bg-transparent p-4 font-light text-base outline-none placeholder:text-grey placeholder:font-normal duration-500 transition-colors focus:border-primary`}
+                onChange={(e) => {
+                  if (Number(e.target.value) < 1) {
+                    e.target.value = "1";
+                  } else if (Number(e.target.value) > 2025) {
+                    e.target.value = "2025";
+                  }
+                  setCarYear(Number(e.target.value));
+                }}
+                style={
+                  (isFieldPreparing || carYear) === 1
+                      ? {
+                        backgroundColor: "rgba(255,0,0,0.025)",
+                        border: "2px solid red",
+                      }
+                      : {}
+                }
             />
           </div>
         </div>
@@ -107,24 +131,24 @@ export default function Update() {
         <div className={styles.contentText}>
           <label className={styles.labelBottom}>Описание</label>
           <textarea
-            value={carDescription}
-            placeholder="Введите описание"
-            onChange={(e) => setCarDescription(e.target.value)}
-            style={
-              (isFieldPreparing || carDescription) === ""
-                ? {
-                    backgroundColor: "rgba(255,0,0,0.025)",
-                    border: "2px solid red",
-                  }
-                : {}
-            }
+              value={carDescription}
+              placeholder="Введите описание"
+              onChange={(e) => setCarDescription(e.target.value)}
+              style={
+                (isFieldPreparing || carDescription) === ""
+                    ? {
+                      backgroundColor: "rgba(255,0,0,0.025)",
+                      border: "2px solid red",
+                    }
+                    : {}
+              }
           ></textarea>
         </div>
         <div className="flex flex-row gap-6 justify-between m-0 flex-wrap">
           <Link href={`${DASHBOARD_PAGES.BUSINESS_CARS}/${carId}`}>
             <button
-              className={`${styles.whiteButton}`}
-              onClick={handleCancelAddCar}
+                className={`${styles.whiteButton}`}
+                onClick={handleCancelAddCar}
             >
               Отменить
             </button>
