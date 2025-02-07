ef76779f37de9ff087338d72ac742b06e31a3ab5
VladKhurs <khursvladislav@gmail.com>
2025-02-07 18:51:15 +0300

    feat: add verification

---
+++ src/app/client/verification/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Verification from "./Verification";
+
+export default function VerificationPage() {
+    return (
+        <div className={`flex justify-center self-center m-auto items-center w-10/12 mb-9`}>
+            <Verification/>
+        </div>
+    );
+};
---
+++ src/services/verification.service.ts
@@ -0,0 +1,24 @@
+import { axiosWithAuth } from "@/api/interceptors";
+
+export const VerificationService = {
+
+    async makeVerification(formData: FormData) {
+      try {
+        const response = await axiosWithAuth.post('/v1/users/verify/', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        if (response.status === 201 && response.data) {
+          console.log('Верификация успешна:', response.data);
+          return response.data;
+        } else {
+          throw new Error('Не удалось верифицировать');
+        }
+      } catch (error) {
+        console.error('Произошла ошибка при верификации:', error);
+        throw error;
+      }
+    },
+  }
