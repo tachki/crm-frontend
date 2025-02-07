de6340dea0ab58a6c0f3f5b4f7938c5e130521e3
Diana Kononovich <149074930+imsiteb@users.noreply.github.com>
2025-02-07 23:30:12 +0300

    Fixed problems w rredirect (#85)

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,14 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/', // Откуда будет происходить редирект
-        destination: '/auth', // Куда будет происходить редирект
-        permanent: false, // Если false, это временный редирект (302)
+        source: '/',
+        destination: '/auth',
+        permanent: true,
       },
     ];
-  },
+  },  
 };
+
 
 module.exports = {
   images: {
