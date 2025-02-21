0a19b03f8b712c0df7d6a03e469226f92c4e4848
VladKhurs <khursvladislav@gmail.com>
2025-02-21 22:27:14 +0300

    feat: update

--- src/app/client/reservations/Reservation.tsx
+++ src/app/client/reservations/Reservation.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import { useGetReservationsByUserId } from "@/services/reservation.service";
 import { CarService } from "@/services/car.service";
-import {CarDto, mapCarDtoToCar} from "@/types/car.type";
 
 interface Car {
     brand: string;
