3f3112a4f1a9809dbc388ddd7b768d24f37614c4
VladKhurs <khursvladislav@gmail.com>
2025-02-21 23:03:48 +0300

    feat: update

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -21,7 +21,7 @@ export const CarService = {
       }
     },
 
-    async getCar(id: string) {
+    async getCar(id: string):Promise<CarDto | GetCarDto> {
       const response = await axiosWithAuth.get<GetCarDto>(`/v1/cars/${id}`);
       return response.data.car;
     },
