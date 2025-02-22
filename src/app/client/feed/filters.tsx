55b4e0bab58421c4911ea3f7fc794af337017f8d
Diana Kononovich <149074930+imsiteb@users.noreply.github.com>
2025-02-22 11:47:28 +0300

    Cnaged select on multi select (#97)

--- src/app/client/feed/filters.tsx
+++ src/app/client/feed/filters.tsx
@@ -1,59 +1,79 @@
-import { carBrandData, carClassData, carTransmissionsData } from '@/utils/constants';
-import React, { useState } from 'react';
-import RangeSlider from 'react-range-slider-input';
-import "react-range-slider-input/dist/style.css";
+import { carBrandData, carClassData, carTransmissionsData } from '@/utils/constants'
+import React, { useState } from 'react'
+import RangeSlider from 'react-range-slider-input'
+import "react-range-slider-input/dist/style.css"
 
 
 interface FiltersProps {
+  filters: {
+    class?: string
+    brand?: string
+    start_date?: string
+    end_date?: string
+    sort?: "prc.d" | "prc.a"
+  }
   setFilters: React.Dispatch<
     React.SetStateAction<{
-      class?: string;
-      brand?: string;
-      start_date?: string;
-      end_date?: string;
-      sort?: "prc.d" | "prc.a";
+      class?: string
+      brand?: string
+      start_date?: string
+      end_date?: string
+      sort?: "prc.d" | "prc.a"
     }>
-  >;
+  >
 }
 
-const Filters: React.FC<FiltersProps> = ({ setFilters }) => {
-  const [showAllBrands, setShowAllBrands] = useState(false);
-  const [showAllClasses, setShowAllClasses] = useState(false);
-  const [sortCriteria, setSortCriteria] = useState<"prc.d" | "prc.a" | undefined>();
-  const [selectedBrand, setSelectedBrand] = useState<string | undefined>();
-  const [selectedClass, setSelectedClass] = useState<string | undefined>();
+const Filters: React.FC<FiltersProps> = ({ filters, setFilters }) => {
+  const [showAllBrands, setShowAllBrands] = useState(false)
+  const [showAllClasses, setShowAllClasses] = useState(false)
+  const [sortCriteria, setSortCriteria] = useState<"prc.d" | "prc.a" | undefined>()
 
-  const toggleShowAllBrands = () => setShowAllBrands(!showAllBrands);
-  const toggleShowAllClasses = () => setShowAllClasses(!showAllClasses);
+  const toggleShowAllBrands = () => setShowAllBrands(!showAllBrands)
+  const toggleShowAllClasses = () => setShowAllClasses(!showAllClasses)
 
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value as "prc.d" | "prc.a";
-    setSortCriteria(value);
-    setFilters((prev) => ({ ...prev, sort: value }));
-  };
+    const value = e.target.value as "prc.d" | "prc.a"
+    setSortCriteria(value)
+    setFilters((prev) => ({ ...prev, sort: value }))
+  }
 
   const handleBrandChange = (brand: string) => {
-    setSelectedBrand(brand); 
-    setFilters((prev) => ({
-      ...prev,
-      brand, 
-    }));
-  };
-  
+    setFilters((prev) => {
+      const updatedBrands = prev.brand?.split(",") || []
+      if (updatedBrands.includes(brand)) {
+        updatedBrands.splice(updatedBrands.indexOf(brand), 1)
+      } else {
+        updatedBrands.push(brand)
+      }
+
+      return {
+        ...prev,
+        brand: updatedBrands.length > 0 ? updatedBrands.join(",") : "",
+      }
+    })
+  }
+
   const handleClassChange = (carClass: string) => {
-    setSelectedClass(carClass); 
-    setFilters((prev) => ({
-      ...prev,
-      class: carClass, 
-    }));
-  };
-  
+    setFilters((prev) => {
+      const updatedClasses = prev.class?.split(",") || []
+      if (updatedClasses.includes(carClass)) {
+        updatedClasses.splice(updatedClasses.indexOf(carClass), 1)
+      } else {
+        updatedClasses.push(carClass)
+      }
+
+      return {
+        ...prev,
+        class: updatedClasses.length > 0 ? updatedClasses.join(",") : "",
+      }
+    })
+  }
 
   return (
     <div>
       <div>
         <label className="block text-xl font-medium text-gray-400 mb-2">Сортировать</label>
-        <select className="border rounded-lg p-2" value={sortCriteria || ""} onChange={handleSortChange}>
+        <select className="border rounded-lg p-2 font-medium" value={sortCriteria || ""} onChange={handleSortChange}>
           <option value="">Без сортировки</option>
           <option value="prc.d">По убыванию цены</option>
           <option value="prc.a">По возрастанию цены</option>
@@ -64,15 +84,16 @@ const Filters: React.FC<FiltersProps> = ({ setFilters }) => {
         <label className="block text-xl font-medium text-gray-400 mt-4">Выберите бренд</label>
         <div className="mt-1 space-y-2">
           {(showAllBrands ? carBrandData : carBrandData.slice(0, 4)).map((brand, index) => (
-            <label key={index} className="flex items-center">
+            <div key={brand} className='flex font-medium'>
               <input
-                type="radio"
-                className="form-radio"
-                checked={selectedBrand === brand}
+                type="checkbox"
+                className="form-checkbox mr-2"
+                checked={(filters.brand || "").split(',').includes(brand)}
                 onChange={() => handleBrandChange(brand)}
+                id={brand}
               />
-              <span className="ml-2 font-medium">{brand}</span>
-            </label>
+              <label htmlFor={brand} key={index}>{brand}</label>
+            </div>
           ))}
           <button
             onClick={toggleShowAllBrands}
@@ -87,15 +108,16 @@ const Filters: React.FC<FiltersProps> = ({ setFilters }) => {
         <label className="block text-xl font-medium text-gray-400 mt-6">Выберите класс</label>
         <div className="mt-1 space-y-2">
           {(showAllClasses ? carClassData : carClassData.slice(0, 4)).map((carClass, index) => (
-            <label key={index} className="flex items-center">
+            <div key={carClass} className='flex font-medium'>
               <input
-                type="radio"
-                className="form-radio"
-                checked={selectedClass === carClass}
-                onChange={() => handleClassChange(carClass)} 
+                type="checkbox"
+                className="form-checkbox mr-2"
+                checked={(filters.class || "").split(",").includes(carClass)}
+                onChange={() => handleClassChange(carClass)}
+                id={carClass}
               />
-              <span className="ml-2 font-medium">{carClass}</span>
-            </label>
+              <label key={index} htmlFor={carClass}>{carClass}</label>
+            </div>
           ))}
           <button
             onClick={toggleShowAllClasses}
@@ -106,9 +128,10 @@ const Filters: React.FC<FiltersProps> = ({ setFilters }) => {
         </div>
       </div>
 
+
       <div>
         <label className="block text-xl font-medium text-gray-400 mt-6">Тип КПП</label>
-        <select className="border rounded-lg p-2">
+        <select className="border rounded-lg p-2 font-medium">
           <option className="font-normal">Не важно</option>
           {carTransmissionsData.map((transmission, index) => (
             <option key={index} className="font-normal">
@@ -145,7 +168,7 @@ const Filters: React.FC<FiltersProps> = ({ setFilters }) => {
         <RangeSlider />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Filters;
+export default Filters
