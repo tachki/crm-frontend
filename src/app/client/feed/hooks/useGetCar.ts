4bf382ba2d57d186f80946c8f98ca7aaa334cf75
Diana Kononovich <149074930+imsiteb@users.noreply.github.com>
2025-03-07 23:45:36 +0300

    Added price and transmission filters (#116)

--- src/app/client/feed/hooks/useGetCar.ts
+++ src/app/client/feed/hooks/useGetCar.ts
@@ -1,37 +1,27 @@
-import { CarService } from "@/services/car.service";
-import { useQuery } from "@tanstack/react-query";
+import { CarService } from "@/services/car.service"
+import { IFilters } from '@/types/car.type'
+import { useQuery } from "@tanstack/react-query"
 
-interface CarsParams {
-    class?: string;
-    brand?: string;
-    start_date?: string;
-    end_date?: string;
-    sort?: "prc.d" | "prc.a";
-    limit?: number;
-    offset?: number;
-  }
-  
+export const useFilteredCars = (filters: IFilters) => {
+  return useQuery({
+    queryKey: ["cars", filters],
+    queryFn: () => CarService.getCarsWithFilters(filters),
+    staleTime: 5 * 60 * 1000,
+  })
+}
 
-export const useFilteredCars = (filters: CarsParams) => {
-    return useQuery({
-      queryKey: ["cars", filters],
-      queryFn: () => CarService.getCarsWithFilters(filters),
-      staleTime: 5 * 60 * 1000, 
-    });
-  };
-
-  export const useFilteredCarsByBusiness = (filters: CarsParams, id: string) => {
-    return useQuery({
-      queryKey: ["cars", filters],
-      queryFn: () => CarService.getBusinessCarsWithFilters(filters, id),
-      staleTime: 5 * 60 * 1000, 
-    });
-  };
+export const useFilteredCarsByBusiness = (filters: IFilters, id: string) => {
+  return useQuery({
+    queryKey: ["business cars", filters],
+    queryFn: () => CarService.getBusinessCarsWithFilters(filters, id),
+    staleTime: 5 * 60 * 1000,
+  })
+}
 
 export const useCar = (carId: string) => {
-    return useQuery({
-      queryKey: ["car", carId],
-      queryFn: () => CarService.getCar(carId),
-      staleTime: 5 * 60 * 1000, 
-    });
-  };
+  return useQuery({
+    queryKey: ["car", carId],
+    queryFn: () => CarService.getCar(carId),
+    staleTime: 5 * 60 * 1000,
+  })
+}
--- src/app/dashboard/business/cars/page.tsx
+++ src/app/dashboard/business/cars/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { mapCarDtoToCar } from "@/types/car.type"
+import { IFilters, mapCarDtoToCar } from "@/types/car.type"
 import { decodeTokens } from "@/services/auth-token.service"
 import { TailSpin } from 'react-loader-spinner'
 import emptyParkImage from '@/images/main_page_park/empty_park.png'
@@ -8,10 +8,9 @@ import plusIcon from '@/images/main_page_park/plus.png'
 import { DASHBOARD_PAGES } from "@/config/pages-url.config"
 import CarCard from './CarCard'
 import Link from 'next/link'
-import Filters from "./filters"
 import { useState } from "react"
 import { useFilteredCarsByBusiness } from "@/app/client/feed/hooks/useGetCar"
-
+import CarsFilters from '@/components/filters/CarsFilters'
 
 export default function Home() {
   const userStorage = decodeTokens()
@@ -20,13 +19,7 @@ export default function Home() {
     businessId = userStorage.business_id
   }
 
-  const [filters, setFilters] = useState<{
-    class?: string
-    brand?: string
-    start_date?: string
-    end_date?: string
-    sort?: "prc.d" | "prc.a"
-  }>({})
+  const [filters, setFilters] = useState<IFilters>({})
 
   const { data: cars = [], isLoading, error } = useFilteredCarsByBusiness(filters, businessId)
 
@@ -35,7 +28,7 @@ export default function Home() {
   return (
     <div className="flex flex-col md:flex-row gap-6 justify-between items-start mb-6">
       <div className="w-full md:w-1/4 bg-white p-4 shadow-md rounded-lg h-auto">
-        <Filters filters={filters} setFilters={setFilters} />
+        <CarsFilters filters={filters} setFilters={setFilters} />
       </div>
       <div className="flex-1 flex flex-col">
         {(cars?.length === 0) && <h1 className="text-2xl font-bold text-center">
