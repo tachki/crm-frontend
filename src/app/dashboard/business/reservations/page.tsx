87c9c23532a39d1d78203ffa19243cb1c3b3e467
Dian A <dianakononovic4@gmail.com>
2025-03-12 21:52:22 +0300

    Added dynamic loading

--- src/app/dashboard/business/reservations/page.tsx
+++ src/app/dashboard/business/reservations/page.tsx
@@ -1,9 +1,13 @@
-import Reservations from "./Reservations";
+import dynamic from 'next/dynamic';
+
+const Reservations = dynamic(() => import('./Reservations'), {
+  loading: () => <p>Загрузка...</p>,
+});
 
 export default function ReservationsPage() {
-    return (
-        <div className={`flex justify-between items-center m-6 mb-9`}>
-            <Reservations />
-        </div>
-    )
+  return (
+    <div className={`flex justify-between items-center m-6 mb-9`}>
+      <Reservations />
+    </div>
+  );
 }
