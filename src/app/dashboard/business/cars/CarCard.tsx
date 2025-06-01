8756fb6eddbd19bb0fe5facc7419d027a55a4255
Roman Chechyotkin <romanchechyotkin@gmail.com>
2025-06-01 14:03:08 +0300

    updated car card and feed

--- src/app/dashboard/business/cars/CarCard.tsx
+++ src/app/dashboard/business/cars/CarCard.tsx
@@ -66,115 +66,105 @@ const CarCard: React.FC<CarCardProps> = ({ car }) => {
   }
 
   return (
-    <div className=" bg-white car-card flex gap-4 shadow-xl rounded-lg overflow-hidden border border-gray-200 h-[420px] pr-5">
-      <div className="flex justify-center items-center mt-5 mb-5 ml-5">
+    <div className="shadow-md rounded-2xl p-4 bg-white h-[420px] flex flex-col relative">
+      <div className="absolute top-4 right-4 z-10">
+        <button
+          className="text-black font-medium py-1 rounded inline-flex items-center bg-white border-none outline-none focus:outline-none"
+          onClick={() => setIsDropdownMenuVisible(!isDropdownMenuVisible)}
+        >
+          {carStatus}
+          <span className={`ml-2 w-3 h-3 rounded-full ${statusStyles[carStatus]}`}></span>
+        </button>
+
+        {isDropdownMenuVisible && (
+          <div className="absolute w-60 right-0 mt-1 shadow-xl rounded-lg bg-white z-20">
+            {carStatusData.map((status) => (
+              <button
+                key={status}
+                className="block w-full text-left px-3 py-2 hover:bg-gray-100 text-black font-medium"
+                onClick={() => updateCarStatus(car.id, status as CarStatus)}
+              >
+                {status}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2 className="text-lg font-semibold leading-snug line-clamp-2">
+          {car.brand} {car.model} ({car.year})
+        </h2>
+        <div className="flex items-center text-gray-400 text-sm mt-1 space-x-4 font-light">
+          <div className="flex items-center">
+            {car.class}
+          </div>
+          <div className="flex items-center">
+            {car.transmission}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-grow mt-3 overflow-hidden">
         <img
           src={car.previewImage}
           alt={`${car.brand} ${car.model}`}
-          className="object-cover rounded-2xl h-full"
-
+          className="w-full h-full object-cover rounded-lg"
         />
       </div>
-      <div className="w-1/2 p-2 flex mt-4 flex-col justify-between h-full relative">
-      <div className="absolute top-[5px] right-[5px] z-10">
-      <button
-      className="text-black font-medium  py-1 rounded inline-flex items-center bg-white  border-none outline-none focus:outline-none"
-      onClick={() => setIsDropdownMenuVisible(!isDropdownMenuVisible)}
-    >
-      {carStatus}
-      <span className={`ml-2 w-3 h-3 rounded-full ${statusStyles[carStatus]}`}></span>
-    </button>
 
-
-      {isDropdownMenuVisible && (
-        <div className="absolute w-60 right-0 mt-1 shadow-xl rounded-lg bg-white z-20">
-          {carStatusData.map((status) => (
-            <button
-              key={status}
-              className="block w-full text-left px-3 py-2 hover:bg-gray-100 text-black font-medium"
-              onClick={() => updateCarStatus(car.id, status as CarStatus)}
-            >
-              {status}
-            </button>
-          ))}
+      <div className="grid grid-cols-2 gap-2 mt-3 text-sm">
+        <div>
+          <span className="text-gray-500">Пробег: </span>
+          <span className="font-medium">{car.totalMileage} км</span>
         </div>
-      )}
+        <div>
+          <span className="text-gray-500">Расход: </span>
+          <span className="font-medium">{car.averageConsumption} л</span>
         </div>
-
-        <h2 className="text-xl font-extrabold text-black mb-2">
-        {car.brand} {car.model} ({car.year})
-      </h2>
-
-        <p className="text-black text-lg font-medium mb-2">
-          {car.class} | {car.transmission}
-        </p>
-
-        <div className="text-base grid grid-cols-1 mb-4">
-          <p>
-            <span className="font-light text-gray-800">Общий пробег: </span>
-            <span className="text-black font-medium">{car.totalMileage} км</span>
-          </p>
-          <p>
-            <span className="font-light text-gray-800">Средний пробег: </span>
-            <span className="text-black font-medium">{car.averageMileage} км</span>
-          </p>
-          <p> <span className="font-light text-gray-800">Средний расход: </span>
-            <span className="text-black font-medium">{car.averageConsumption} л</span>
-          </p>
-          <p>
-            <span className="font-light text-gray-800">Сумма затрат: </span>
-            <span className="text-black font-medium">{car.totalExpenses} BYN</span>
-          </p>
-          <p>
-            <span className="font-light text-gray-800">Рейтинг: </span>
-            <span className="text-black font-medium">{car.rating}</span>
-          </p>
-          <p>
-            <span className="font-light text-gray-800">Коэфф. простоя: </span>
-            <span className="text-black font-medium">{car.downtimeCoefficient}</span>
-          </p>
-          <p>
-            <span className="font-light text-gray-800">Цена в сутки: </span>
-            <span className="text-black font-medium">{car.pricePerDay} BYN</span>
-          </p>
+        <div>
+          <span className="text-gray-500">Рейтинг: </span>
+          <span className="font-medium">{car.rating}</span>
         </div>
+        <div>
+          <span className="text-gray-500">Цена: </span>
+          <span className="font-medium">{car.pricePerDay} BYN</span>
+        </div>
+      </div>
 
-        <div className="buttons flex gap-2 justify-between pt-2 mb-7">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded shadow hover:bg-blue-600 font-medium text-sm w-1/3"
-          >
-            <Link href={`${DASHBOARD_PAGES.BUSINESS_CARS}/${car.id}`}>
-              Подробнее
-            </Link>
+      <div className="flex justify-between items-center gap-2 mt-4">
+        <Link href={`${DASHBOARD_PAGES.BUSINESS_CARS}/${car.id}`} className="flex-1">
+          <button className="w-full bg-blue-600 text-white text-sm font-medium py-2 px-4 rounded-lg">
+            Подробнее
           </button>
-          <button
-            ref={calendarButtonRef}
-            onClick={toggleCalendarVisibility}
-            className="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-600 font-medium text-sm w-1/3 flex items-center justify-center gap-2"
-          >
-            <Image
-              src={calendarIcon.src}
-              alt="Календарь"
-              className="w-5 h-5"
-              width={120}
-              height={120}
-            />
-            Календарь
-          </button>
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded shadow hover:bg-red-600 font-medium text-sm w-1/3 flex items-center justify-center gap-2"
-            onClick={openDeleteModal}
-          >
-            <Image
-              src={deleteIcon.src}
-              alt="Удалить"
-              className="w-5 h-5"
-              width={120}
-              height={120}
-            />
-            Удалить
-          </button>
-        </div>
+        </Link>
+        <button
+          ref={calendarButtonRef}
+          onClick={toggleCalendarVisibility}
+          className="flex-1 bg-green-500 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center justify-center gap-2"
+        >
+          <Image
+            src={calendarIcon.src}
+            alt="Календарь"
+            className="w-4 h-4"
+            width={16}
+            height={16}
+          />
+          Календарь
+        </button>
+        <button
+          className="flex-1 bg-red-500 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center justify-center gap-2"
+          onClick={openDeleteModal}
+        >
+          <Image
+            src={deleteIcon.src}
+            alt="Удалить"
+            className="w-4 h-4"
+            width={16}
+            height={16}
+          />
+          Удалить
+        </button>
       </div>
 
       {isCalendarVisible && (
--- src/app/dashboard/business/cars/CarsFeed.tsx
+++ src/app/dashboard/business/cars/CarsFeed.tsx
@@ -2,7 +2,6 @@
 
 import { IFilters, mapCarDtoToCar } from "@/types/car.type"
 import { decodeTokens } from "@/services/auth-token.service"
-import { TailSpin } from 'react-loader-spinner'
 import emptyParkImage from '@/images/main_page_park/empty_park.png'
 import plusIcon from '@/images/main_page_park/plus.png'
 import { DASHBOARD_PAGES } from "@/config/pages-url.config"
@@ -13,7 +12,7 @@ import { useFilteredCarsByBusiness } from "@/app/client/feed/hooks/useGetCar"
 import CarsFilters from '@/components/filters/CarsFilters'
 import Image from 'next/image'
 
-export default function Home() {
+export default function CarsFeed() {
 	const userStorage = decodeTokens()
 	let businessId: string = 'default-business-id'
 	if (userStorage && userStorage.business_id !== undefined) {
@@ -21,67 +20,65 @@ export default function Home() {
 	}
 
 	const [filters, setFilters] = useState<IFilters>({})
-
 	const { data: cars = [], isLoading, error } = useFilteredCarsByBusiness(filters, businessId)
 
-	if (error) return <p>Ошибка загрузки </p>
+	if (error) {
+		return (
+			<div className="flex justify-center items-center h-full">
+				<p className="text-red-500 text-lg font-medium">Ошибка загрузки данных</p>
+			</div>
+		)
+	}
 
 	return (
-		<div className="flex flex-col md:flex-row gap-6 justify-between items-start mb-6">
-			<div className="w-full md:w-1/4 bg-white p-4 shadow-md rounded-lg h-auto">
-				<CarsFilters filters={filters} setFilters={setFilters} />
-			</div>
-			<div className="flex-1 flex flex-col">
-				{(cars?.length === 0) && <h1 className="text-2xl font-bold text-center">
-					Кажется, здесь пока что пусто...
-				</h1>}
-
-				{isLoading ? (
-					<TailSpin
-						height="80"
-						width="80"
-						color="#3B44FF"
-						ariaLabel="loading"
-					/>
-				) : (
-					<p></p>
-				)}
-
-				{error && <p className="text-red-500">Ошибка: {error}</p>}
-
-				{!isLoading && !error && cars?.length === 0 && (
-					<div className="flex flex-col items-center justify-center">
-						<Image
-							src={emptyParkImage.src}
-							alt="Нет автомобилей"
-							width={550}
-							height={350}
-							className="mb-4"
-						/>
-
-						<p className="text-lg font-medium text-black">Добавьте автомобили для их отображения на странице</p>
-					</div>
-				)}
-
-				<div className="grid grid-cols-1 gap-4">
-					{cars.map((car) => (
-						<CarCard key={car.id} car={mapCarDtoToCar(car)} />
-					))}
+		<div className="mx-auto">
+			<div className="flex flex-col md:flex-row gap-6 justify-between items-start mb-6">
+				<div className="w-full md:w-1/4 bg-white p-4 shadow-md rounded-lg h-auto">
+					<CarsFilters filters={filters} setFilters={setFilters} />
 				</div>
 
-				<Link href={DASHBOARD_PAGES.CREATE}>
-					<div
-						className="fixed bottom-10 right-10 bg-yellow-500 rounded-full w-16 h-16 flex items-center justify-center shadow-lg"
-					>
-						<Image
-							src={plusIcon.src}
-							alt="Plus Icon"
-							width={18}
-							height={18}
-						/>
-					</div>
-				</Link>
+				<div className="flex-1 flex flex-col">
+					{isLoading ? (
+						<div className="flex justify-center items-center h-full">
+							<p className="text-lg font-medium">Загрузка...</p>
+						</div>
+					) : (
+						<>
+							{cars.length === 0 ? (
+								<div className="flex flex-col items-center justify-center text-center">
+									<Image
+										src={emptyParkImage.src}
+										alt="Нет автомобилей"
+										width={550}
+										height={350}
+										className="mb-4"
+									/>
+									<p className="text-lg font-medium text-black">
+										Добавьте автомобили для их отображения на странице
+									</p>
+								</div>
+							) : (
+								<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
+									{cars.map((car) => (
+										<CarCard key={car.id} car={mapCarDtoToCar(car)} />
+									))}
+								</div>
+							)}
+						</>
+					)}
+				</div>
 			</div>
+
+			<Link href={DASHBOARD_PAGES.CREATE}>
+				<div className="fixed bottom-10 right-10 bg-yellow-500 rounded-full w-16 h-16 flex items-center justify-center shadow-lg">
+					<Image
+						src={plusIcon.src}
+						alt="Добавить автомобиль"
+						width={18}
+						height={18}
+					/>
+				</div>
+			</Link>
 		</div>
 	)
 }
